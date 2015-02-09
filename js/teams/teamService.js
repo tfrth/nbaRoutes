be42abec88098778fd7c13d4268a20ececca733b
Ty Frith <tyfrith@gmail.com>
2015-02-09 00:05:52 -0700

    some progress

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,57 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+	this.addNewGame = function(gameObj) {
+		var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam;
+		
+	if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+		gameObj.won = true;
+	}	else {
+		gameObj.win = false;
+	}
+	return $http ({
+		method: 'POST',
+		url: url,
+		data: gameObj
+		})
+	};
+
+
+	this.getTeamData = function(team) {
+		var dfd = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+		console.log(team)
+		$http({
+			method: 'GET',
+			url: url
+		}).then(function(data) {
+			var res = data.data.results;
+			var wins = 0;
+			var losses = 0;
+		for (var i = 0; i < res.length; i++) {
+			if (res[i].won === true) {
+				wins += 1;
+		} else if(res[i].won === false) {
+			losses += 1;
+		}
+	}
+		res.wins = wins;
+		res.losses = losses;
+		console.log(res);
+		dfd.resolve(res);	
+		
+	})
+
+		return defered.promise;
+	}
+
 });
+
+
+
+
+
+
+
+//--------------------------------------
+
